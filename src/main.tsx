1e2591b37aa550742e50ecc2e89a495d50135ca7
ibrahimfares511 <ibrahimfares511@gmail.com>
2026-06-10 01:09:46 +0300

    add @vercel/speed-insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App'
 import { ThemeProvider } from '@/components/shared/ThemeContext'
 import './lib/i18n'
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <App />
           <Analytics/>
+          <SpeedInsights/>
         </BrowserRouter>
       </ThemeProvider>
     </HelmetProvider>
