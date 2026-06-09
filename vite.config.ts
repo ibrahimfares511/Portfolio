a1a6f4b77485a64e86d6cf0104babee108a4e29d
ibrahimfares511 <ibrahimfares511@gmail.com>
2026-06-09 21:44:41 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from 'autoprefixer'
 import path from 'path'
 
 export default defineConfig({
@@ -20,4 +21,12 @@ export default defineConfig({
       },
     },
   },
+  css: {
+    transformer: 'postcss',
+    postcss: {
+      plugins: [
+        autoprefixer({ remove: false })
+      ]
+    }
+  }
 })
