da2f9698ead8f0379a0041753cb0a82c727832a6
ibrahimfares511 <ibrahimfares511@gmail.com>
2026-06-10 01:03:46 +0300

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import App from './App'
 import { ThemeProvider } from '@/components/shared/ThemeContext'
 import './lib/i18n'
